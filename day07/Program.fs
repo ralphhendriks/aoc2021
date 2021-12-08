fb1403d105e1354b6c04a009a4b8860fd0fd60aa
Ralph Hendriks <mail@ralphhendriks.nl>
2021-12-08 09:37:21 +0100

    Simplify day 7

--- day07/Program.fs
+++ day07/Program.fs
@@ -3,26 +3,6 @@
     |> (fun s -> s.Split(','))
     |> Array.map int
     |> Array.toList
- 
-let minimumFuel =
-    [(List.min positions) .. (List.max positions)]
-    |> List.map (fun r ->
-        positions
-        |> List.map (fun y -> abs (y - r))
-        |> List.sum)
-    |> List.min
-
-printfn "%A" minimumFuel
-
-let minimumFuel2 =
-    [(List.min positions) .. (List.max positions)]
-    |> List.map (fun r ->
-        positions
-        |> List.map (fun y ->
-            let d = abs (y - r)
-            d * (d + 1) / 2)
-        |> List.sum)
-    |> List.min
 
 let linearFuelRate x y = abs (x - y)
 
@@ -38,6 +18,5 @@ let findMinimumFuel fuelAlgorithm =
         |> List.sum)
     |> List.min
 
-printfn "%A" minimumFuel2
 printfn "Answer part 1: %i" (findMinimumFuel linearFuelRate)
 printfn "Answer part 2: %i" (findMinimumFuel increasingFuelRate)
