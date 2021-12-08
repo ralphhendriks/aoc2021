c035ab39e56f475e6b42133c7f302d889832393b
Ralph Hendriks <mail@ralphhendriks.nl>
2021-12-08 09:42:37 +0100

    Simplify day 7 even further

--- day07/Program.fs
+++ day07/Program.fs
@@ -2,7 +2,6 @@
     System.IO.File.ReadAllText("input.txt")
     |> (fun s -> s.Split(','))
     |> Array.map int
-    |> Array.toList
 
 let linearFuelRate x y = abs (x - y)
 
@@ -11,11 +10,8 @@ let increasingFuelRate x y =
     d * (d + 1) / 2
 
 let findMinimumFuel fuelAlgorithm =
-    [(List.min positions) .. (List.max positions)]
-    |> List.map (fun r ->
-        positions
-        |> List.map (fun p -> fuelAlgorithm r p)
-        |> List.sum)
+    [(Array.min positions) .. (Array.max positions)]
+    |> List.map (fun r -> positions |> Array.map (fuelAlgorithm r) |> Array.sum)
     |> List.min
 
 printfn "Answer part 1: %i" (findMinimumFuel linearFuelRate)
